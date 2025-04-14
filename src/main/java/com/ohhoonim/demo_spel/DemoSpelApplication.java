daf34b8996e4527cd2eaddad53e126bb8b593b4d
주형돈 <hdju@rntime.com>
2025-04-14 18:52:02 +0900

    init

---
+++ src/main/java/com/ohhoonim/demo_spel/DemoSpelApplication.java
@@ -0,0 +1,13 @@
+package com.ohhoonim.demo_spel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohhoonim/demo_spel/DemoSpelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohhoonim.demo_spel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
